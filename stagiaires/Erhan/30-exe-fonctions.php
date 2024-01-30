14d41fb298966c9ff3c0afde8772805725993c98
erdaserhan <erhan209@gmail.com>
2024-01-30 14:20:52 +0100

    Correction

--- stagiaires/Erhan/30-exe-fonctions.php
+++ stagiaires/Erhan/30-exe-fonctions.php
@@ -2,32 +2,13 @@
 
 // 1) Créez une fonction qui prend en paramètre un nombre et qui retourne le carré de ce nombre.
 
-function nbSquare($num1) {
-    return $num1 * $num1;
-}
-
 // puis affichez le carré de 5, 10 et 15 en utilisant cette fonction.
-echo nbSquare(5);
-echo "<hr>";
-echo nbSquare(10);
-echo "<hr>";
-echo nbSquare(15);
-echo "<hr>";
 
 
 // 2) Créez une fonction qui prend en paramètre un nombre et qui retourne le cube de ce nombre.
 
-function nbCube($num1) {
-    return $num1 * $num1 * $num1;
-}
 
 // puis affichez le cube de 5, 10 et 15 en utilisant cette fonction.
-
-echo nbCube(5);
-echo "<hr>";
-echo nbCube(10);
-echo "<hr>";
-echo nbCube(15);
 
 // 3) Créez une fonction qui prend une date à n'importe quel format et qui retourne la date au format français
 // telle qu'indiquée dans l'exemple en utilisant $semaineFr et $moisFr.
@@ -42,11 +23,6 @@ function dateEnFr($date){
     $moisFr=[1 =>'janvier','février','mars','avril','mai','juin','juillet',
         'août','septembre','octobre','novembre','décembre',];
     // A FAIRE
-
-    echo strtotime('l jS \of F Y h:i:s A');
-    echo $jourActuel = date('N');
-    echo "{$semaineFr[$jourActuel]}.";
-
 }
 echo dateEnFr('2021-12-25 12:27:35');
 echo '<br>';
